45529e097f7c7ad2e168c04de545f4bfa739222e
Marwan-Alghandour <marwanalghandour23@gmail.com>
2024-04-14 22:31:13 +0200

    Fixing bug

--- client/src/hooks/useLikeMedia.ts
+++ client/src/hooks/useLikeMedia.ts
@@ -1,14 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import APIClient from "../services/apiClient";
 
 const useLikeMedia = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ id, liked }: { id: number; liked: boolean }) => {
       const apiClient = new APIClient<{ liked: boolean }>(`/like/${id}`);
 
       return apiClient.put({ liked });
     },
-    // onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["media"] });
+    },
   });
 };
 
